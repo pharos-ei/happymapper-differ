cd904378f87edd3a31b6d00b3c800e9f46af54cf
John Weir <john@smokinggun.com>
2016-03-07 16:12:01 -0800

    move UnExtendable into its own file

--- lib/happymapper/differ.rb
+++ lib/happymapper/differ.rb
@@ -70,14 +70,6 @@ module HappyMapper
     end
   end
 
-  # nil, Float, and other classes can't be extended
-  # so this object acts as wrapper
-  class UnExtendable < SimpleDelegator
-    def class
-      __getobj__.class
-    end
-  end
-
   # DiffedItem is an extension which allows tracking changes between two
   # HappyMapper objects.
   module DiffedItem
--- lib/happymapper_differ.rb
+++ lib/happymapper_differ.rb
@@ -3,5 +3,6 @@ require 'happymapper'
 # HappyMapper module imported from the nokogiri-happymapper gem
 module HappyMapper
   require 'happymapper/differ'
+  require 'happymapper/un_extendable'
 end
 
